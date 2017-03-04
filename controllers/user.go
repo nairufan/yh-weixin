7670063bd461dbc28ade61f7c405de614977e744
unknown <fannairu637@sina.com>
2017-03-04 11:08:46 +0800

    add sessionId in login response

--- controllers/user.go
+++ controllers/user.go
@@ -4,7 +4,15 @@ type UserController struct {
 	BaseController
 }
 
+type loginResponse struct {
+	SessionId string     `json:"sessionId"`
+}
 // @router /mock-login [get]
 func (u *UserController) MockLogin() {
 	u.SetUserId("1111")
+	response := &loginResponse{
+		SessionId: u.CruSession.SessionID(),
+	}
+	u.Data["json"] = response
+	u.ServeJSON()
 }
